ec02afbff9dcc185f7de6427e0ffcaedc13d6102
Nikhil <sreenik111@gmail.com>
2021-10-18 15:04:53 +0530

    initial commit

---
+++ TestApp/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TestApp
+//
+//  Created by qbuser on 18/10/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window:UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
