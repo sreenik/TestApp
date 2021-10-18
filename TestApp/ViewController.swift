235a2a0a1d2d1028f8003006ea6a1c65b1a79e95
Nikhil <sreenik111@gmail.com>
2021-10-18 17:55:55 +0530

    test

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Welcome")
         //new test
         //test
         // Do any additional setup after loading the view.
